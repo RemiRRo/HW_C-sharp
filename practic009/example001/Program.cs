5c479289d0fa7e55ee5e586f6dd7b3f01057ac58
Vladimir Cherezov <remirrorap@gmail.com>
2023-03-11 21:53:54 +0300

    practic 9

---
+++ practic009/example001/Program.cs
@@ -0,0 +1,26 @@
+﻿int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+if (n < 1 || m < 1)
+{
+    Console.WriteLine("Ввели не положительное число");
+}
+if (m < n) {
+    Console.WriteLine(NaturalNumber(m, n));
+} else {
+    Console.WriteLine(NaturalNumber(n, m));
+}
+
+int NaturalNumber(int m, int n)
+{
+    if (n == m)
+        return n;
+    if (m < n && n % 2 == 0)
+        Console.WriteLine(n);
+        return NaturalNumber(m, n - 1);
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
---
+++ practic009/example003/Program.cs
@@ -0,0 +1,17 @@
+﻿int m = InputInt("Введите M:");
+int n = InputInt("Введите N:");
+Console.WriteLine($"НОД = {GreatestCommonFactor(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int GreatestCommonFactor(int m, int n)
+{
+    if (n == 0)
+        return m;
+    else 
+        return GreatestCommonFactor(n, m % n);
+}
